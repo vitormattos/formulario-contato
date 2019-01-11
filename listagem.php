10f9a7ff34a6485b8dfa94dede7112205b3da984
jonatas <jonatas@gmail.com>
2019-01-11 19:23:14 -0200

    listagem

--- listagem.php
+++ listagem.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Listagem de Contato</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <script src="main.js"></script>
+</head>
+<body>
+
 <?php
 $pdo = new PDO('sqlite:contatos.sqlite3');
 $sth = $pdo->prepare(
@@ -5,12 +18,15 @@ $sth = $pdo->prepare(
 );
 $sth->execute();
 ?>
+<div>
+<h2><b>Listagem de Contatos</b></h2>
 <table>
   <thead>
     <tr>
       <th>Nome</th>
       <th>Telefone</th>
       <th>Mensagem</th>
+      <th>Opções</th>
       <tr>&nbsp;</th>
     </tr>
   </thead>
@@ -23,13 +39,17 @@ $sth->execute();
         <td><?php echo $row['telefone']?></td>
         <td><?php echo $row['mensagem']?></td>
         <td>
-          <a href="excluir.php?id=<?php
-            echo $row['id'];?>">X</a>
-          <a href="editar.php?id=<?php
-            echo $row['id'];?>">E</a>
+          <a class="btn btn-danger" href="excluir.php?id=<?php
+            echo $row['id'];?>">Excluir</a>
+          <a class="btn btn-primary" href="editar.php?id=<?php
+            echo $row['id'];?>">Editar</a>
         </td>
       </tr><?php
   }
   ?>
   </tbody>
 </table>
+</div>
+
+</body>
+</html>
